f91c6ec362105314144f5fca4bb0879a759e7b74
jok3r46 <jsuf046@gmail.com>
2022-07-05 15:31:52 +0700

    solving conflict

--- features/comments/data/model.go
+++ features/comments/data/model.go
@@ -2,48 +2,49 @@ package data
 
 import (
 	"project3/eventapp/features/comments"
-	"time"
+	_event "project3/eventapp/features/events/data"
+	_user "project3/eventapp/features/users/data"
 
 	"gorm.io/gorm"
 )
 
 type Comment struct {
 	gorm.Model
-	IdEvent int
-	IdUser  int
+	EventID int
+	UserID  int
 	Comment string `gorm:"text" json:"text" binding:"required"`
-	Event   Event
-	User    User
+	Event   _event.Event
+	User    _user.User
 }
 
-type Event struct {
-	gorm.Model
-	Name       string    `json:"name" form:"name"`
-	Detail     string    `json:"detail" form:"detail"`
-	URL        string    `json:"url" form:"url"`
-	Date       time.Time `json:"time" form:"time"`
-	Performers string    `json:"performers" form:"performers"`
-	HostedBy   string    `json:"hostedby" form:"hostedby"`
-	City       string    `json:"city" form:"city"`
-	Location   string    `json:"location" form:"location"`
-	IDUser     int
-	User       User
-	Comment    []Comment
-}
+// type Event struct {
+// 	gorm.Model
+// 	Name       string    `json:"name" form:"name"`
+// 	Detail     string    `json:"detail" form:"detail"`
+// 	URL        string    `json:"url" form:"url"`
+// 	Date       time.Time `json:"time" form:"time"`
+// 	Performers string    `json:"performers" form:"performers"`
+// 	HostedBy   string    `json:"hostedby" form:"hostedby"`
+// 	City       string    `json:"city" form:"city"`
+// 	Location   string    `json:"location" form:"location"`
+// 	UserID     int
+// 	User       User
+// 	Comment    []Comment
+// }
 
-type User struct {
-	gorm.Model
-	Name     string
-	Email    string
-	Password string
-	Event    []Event
-}
+// type User struct {
+// 	gorm.Model
+// 	Name     string
+// 	Email    string
+// 	Password string
+// 	Event    []Event
+// }
 
 func (data *Comment) toCore() comments.Core {
 	return comments.Core{
 		ID:        int(data.ID),
-		IdEvent:   data.IdEvent,
-		IdUser:    data.IdUser,
+		IdEvent:   data.EventID,
+		IdUser:    data.UserID,
 		Comment:   data.Comment,
 		CreatedAt: data.CreatedAt,
 		UpdatedAt: data.UpdatedAt,
--- features/participants/data/model.go
+++ features/participants/data/model.go
@@ -1,49 +1,49 @@
 package data
 
 import (
+	_event "project3/eventapp/features/events/data"
 	"project3/eventapp/features/participants"
-	"time"
+	_user "project3/eventapp/features/users/data"
 
 	"gorm.io/gorm"
 )
 
 type Participant struct {
 	gorm.Model
-	IdUser  int
-	IdEvent int
-	Event   Event
-	User    User
+	UserID  int
+	EventID int
+	Event   _event.Event
+	User    _user.User
 }
 
-type Event struct {
-	gorm.Model
-	Name        string    `json:"name" form:"name"`
-	Detail      string    `json:"detail" form:"detail"`
-	URL         string    `json:"url" form:"url"`
-	Date        time.Time `json:"time" form:"time"`
-	Performers  string    `json:"performers" form:"performers"`
-	HostedBy    string    `json:"hostedby" form:"hostedby"`
-	City        string    `json:"city" form:"city"`
-	Location    string    `json:"location" form:"location"`
-	IDUser      int
-	User        User
-	Participant []Participant
-}
+// type Event struct {
+// 	gorm.Model
+// 	Name        string    `json:"name" form:"name"`
+// 	Detail      string    `json:"detail" form:"detail"`
+// 	URL         string    `json:"url" form:"url"`
+// 	Date        time.Time `json:"time" form:"time"`
+// 	Performers  string    `json:"performers" form:"performers"`
+// 	HostedBy    string    `json:"hostedby" form:"hostedby"`
+// 	City        string    `json:"city" form:"city"`
+// 	Location    string    `json:"location" form:"location"`
+// 	UserID      int
+// 	User        User
+// 	Participant []Participant
+// }
 
-type User struct {
-	gorm.Model
-	Name        string
-	Email       string
-	Password    string
-	Event       []Event
-	Participant []Participant
-}
+// type User struct {
+// 	gorm.Model
+// 	Name     string
+// 	Email    string
+// 	Password string
+// 	Event    []Event
+// }
 
 func (data *Participant) toCore() participants.Core {
 	return participants.Core{
 		ID:        int(data.ID),
-		IdEvent:   data.IdEvent,
-		IdUser:    data.IdUser,
+		IdEvent:   data.EventID,
+		IdUser:    data.EventID,
 		CreatedAt: data.CreatedAt,
 		UpdatedAt: data.UpdatedAt,
 	}
