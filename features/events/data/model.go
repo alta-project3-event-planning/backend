b51b3e4c2e4a1533b68d0a1749ab79427574dfa1
Alfin7007 <alfin.7007@gmail.com>
2022-07-05 02:51:24 -0400

    repair model event userid

--- features/events/data/model.go
+++ features/events/data/model.go
@@ -3,7 +3,6 @@ package data
 import (
 	"project3/eventapp/features/events"
 	"project3/eventapp/features/users/data"
-	"time"
 
 	"gorm.io/gorm"
 )
@@ -13,13 +12,20 @@ type Event struct {
 	Name       string    `json:"name" form:"name"`
 	Detail     string    `json:"detail" form:"detail"`
 	URL        string    `json:"url" form:"url"`
-	Date       time.Time `json:"time" form:"time"`
+	Date       string	 `json:"time" form:"time"`
 	Performers string    `json:"performers" form:"performers"`
 	HostedBy   string    `json:"hostedby" form:"hostedby"`
 	City       string    `json:"city" form:"city"`
 	Location   string    `json:"location" form:"location"`
-	IDUser     int
+	UserID     int
 	User       data.User
+}
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
 }
 
 //DTO
@@ -34,7 +40,7 @@ func (data *Event) toCore() events.Core {
 		HostedBy:    data.HostedBy,
 		Performers:  data.Performers,
 		Location:    data.Location,
-		IDUser:      data.IDUser,
+		IDUser:      data.UserID,
 	}
 }
 
@@ -53,7 +59,7 @@ func fromCore(core events.Core) Event {
 		URL:        core.Url,
 		HostedBy:   core.HostedBy,
 		Performers: core.Performers,
-		IDUser:     core.IDUser,
+		UserID:     core.IDUser,
 		User: data.User{
 			Name:  core.User.Name,
 			Email: core.User.Email,
